7aaebe53bf992bdcdd0f108b7e4c6c92b32dd2ce
LucyMulei <muleilucy@gmail.com>
2024-07-17 11:42:38 +0300

    Done stuff

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+document.addEventListener("keydown", function (event) {
+  console.log(event);
+});
+const dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
